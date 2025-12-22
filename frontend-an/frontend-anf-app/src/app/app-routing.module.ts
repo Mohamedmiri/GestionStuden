74409155ee7f65e358c9c93d68fbc99a9ff5c482
Mohamedmiri <mirimohamed003@gmail.com>
2025-12-22 17:07:52 +0100

    add the component

--- frontend-an/frontend-anf-app/src/app/app-routing.module.ts
+++ frontend-an/frontend-anf-app/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { LoadStudentsComponent } from './load-students/load-students.component';
+import { LoadPaymentsComponent } from './load-payments/load-payments.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentsComponent } from './students/students.component';
+import { PaymentsComponent } from './payments/payments.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'loadStudents', component: LoadStudentsComponent },
+  { path: 'loadPayments', component: LoadPaymentsComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'payments', component: PaymentsComponent },
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend-an/frontend-anf-app/src/app/app.module.ts
+++ frontend-an/frontend-anf-app/src/app/app.module.ts
@@ -4,14 +4,49 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoadStudentsComponent } from './load-students/load-students.component';
+import { LoadPaymentsComponent } from './load-payments/load-payments.component';
+import { LoginComponent } from './login/login.component';
+import { StudentsComponent } from './students/students.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminTemplateComponent,
+    HomeComponent,
+    ProfileComponent,
+    LoadStudentsComponent,
+    LoadPaymentsComponent,
+    LoginComponent,
+    StudentsComponent,
+    PaymentsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule
+
+
   ],
   providers: [
     provideClientHydration(),
